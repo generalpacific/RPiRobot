9de0edd88b6b8fc2da33cc80923700d132bdac40
generalpacific <prash.c.29@gmail.com>
2023-05-13 13:04:34 -0700

    Format the python files

--- rpirobot/keyboard_controlled_robot.py
+++ rpirobot/keyboard_controlled_robot.py
@@ -1,61 +1,66 @@
-import sys, tty, termios
-import robotGPIO as robot 
+import robotGPIO as robot
+import sys
+import termios
+import tty
+
 
 # This script contains logic for controlling the robot
 # using keyboard arrow keys.
 
 class _Getch:
-  def __call__(self):
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-      tty.setraw(sys.stdin.fileno())
-      ch = sys.stdin.read(3)
-    finally:
-      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+    def __call__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
 
 def get():
-  inkey = _Getch()
-  while(1):
-    k=inkey()
-    if k!='':
-      break
-  if k=='\x1b[A':
-    print( "up")
-    robot.forward();
-  elif k=='\x1b[B':
-    print( "down")
-    robot.reverse();
-  elif k=='\x1b[C':
-    print( "right")
-    robot.right()
-  elif k=='\x1b[D':
-    print( "left")
-    robot.left()
-  else:
-    if k=='rrr':
-      print( "rotateRight")
-      robot.rotateRight()
-    elif k=='rrl':
-      print( "rotateLeft")
-      robot.rotateLeft()
-    else :
-      print( "not an arrow key!")
-      return True
-  return False
+    inkey = _Getch()
+    while (1):
+        k = inkey()
+        if k != '':
+            break
+    if k == '\x1b[A':
+        print("up")
+        robot.forward();
+    elif k == '\x1b[B':
+        print("down")
+        robot.reverse();
+    elif k == '\x1b[C':
+        print("right")
+        robot.right()
+    elif k == '\x1b[D':
+        print("left")
+        robot.left()
+    else:
+        if k == 'rrr':
+            print("rotateRight")
+            robot.rotateRight()
+        elif k == 'rrl':
+            print("rotateLeft")
+            robot.rotateLeft()
+        else:
+            print("not an arrow key!")
+            return True
+    return False
 
 
 def main():
-  robot.cleanUp()
-  robot.setupGPIO()
-  while True:
-    is_exit = get()
-    if is_exit == True:
-      print("Exit is true.")
-      break
-  print("Cleaning up state.")
-  robot.cleanUp()
+    robot.cleanUp()
+    robot.setupGPIO()
+    while True:
+        is_exit = get()
+        if is_exit == True:
+            print("Exit is true.")
+            break
+    print("Cleaning up state.")
+    robot.cleanUp()
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- rpirobot/robotGPIO.py
+++ rpirobot/robotGPIO.py
@@ -10,84 +10,98 @@ motorLeftOut1 = 26
 motorLeftOut2 = 19
 
 # Interval between each forward and backward activations. 
-interval = 0.5 
+interval = 0.5
 
 # Interval between each right / left activations. 
 # Eventually controls how much the robot turns for each command.
-turninterval = 0.3 
+turninterval = 0.3
+
 
 def forwardMotorRight():
-  GPIO.output(motorRightOut1, GPIO.HIGH)
-  GPIO.output(motorRightOut2, GPIO.LOW)
+    GPIO.output(motorRightOut1, GPIO.HIGH)
+    GPIO.output(motorRightOut2, GPIO.LOW)
+
 
 def stopMotorRight():
-  GPIO.output(motorRightOut1, GPIO.LOW)
-  GPIO.output(motorRightOut2, GPIO.LOW)
+    GPIO.output(motorRightOut1, GPIO.LOW)
+    GPIO.output(motorRightOut2, GPIO.LOW)
+
 
 def forwardMotorLeft():
-  GPIO.output(motorLeftOut1, GPIO.HIGH)
-  GPIO.output(motorLeftOut2, GPIO.LOW)
+    GPIO.output(motorLeftOut1, GPIO.HIGH)
+    GPIO.output(motorLeftOut2, GPIO.LOW)
+
 
 def stopMotorLeft():
-  GPIO.output(motorLeftOut1, GPIO.LOW)
-  GPIO.output(motorLeftOut2, GPIO.LOW)
+    GPIO.output(motorLeftOut1, GPIO.LOW)
+    GPIO.output(motorLeftOut2, GPIO.LOW)
+
 
 def reverseMotorRight():
-  GPIO.output(motorRightOut2, GPIO.HIGH)
-  GPIO.output(motorRightOut1, GPIO.LOW)
+    GPIO.output(motorRightOut2, GPIO.HIGH)
+    GPIO.output(motorRightOut1, GPIO.LOW)
+
 
 def reverseMotorLeft():
-  GPIO.output(motorLeftOut2, GPIO.HIGH)
-  GPIO.output(motorLeftOut1, GPIO.LOW)
+    GPIO.output(motorLeftOut2, GPIO.HIGH)
+    GPIO.output(motorLeftOut1, GPIO.LOW)
+
 
 def right():
-  forwardMotorLeft()
-  sleep(turninterval)
-  stopMotorLeft()
+    forwardMotorLeft()
+    sleep(turninterval)
+    stopMotorLeft()
+
 
 def left():
-  forwardMotorRight()
-  sleep(turninterval)
-  stopMotorRight()
+    forwardMotorRight()
+    sleep(turninterval)
+    stopMotorRight()
+
 
 def reverse():
-  reverseMotorRight()
-  reverseMotorLeft()
-  sleep(interval)
-  stopMotorRight()
-  stopMotorLeft()
+    reverseMotorRight()
+    reverseMotorLeft()
+    sleep(interval)
+    stopMotorRight()
+    stopMotorLeft()
+
 
 def forward():
-  forwardMotorRight()
-  forwardMotorLeft()
-  sleep(interval)
-  stopMotorRight()
-  stopMotorLeft()
+    forwardMotorRight()
+    forwardMotorLeft()
+    sleep(interval)
+    stopMotorRight()
+    stopMotorLeft()
+
 
 def rotateRight():
-  reverseMotorRight()
-  forwardMotorLeft()
-  sleep(interval)
-  stopMotorRight()
-  stopMotorLeft()
+    reverseMotorRight()
+    forwardMotorLeft()
+    sleep(interval)
+    stopMotorRight()
+    stopMotorLeft()
+
 
 def rotateLeft():
-  forwardMotorRight()
-  reverseMotorLeft()
-  sleep(interval)
-  stopMotorRight()
-  stopMotorLeft()
+    forwardMotorRight()
+    reverseMotorLeft()
+    sleep(interval)
+    stopMotorRight()
+    stopMotorLeft()
+
 
 def setupGPIO():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(motorRightOut1, GPIO.OUT)
-  GPIO.setup(motorRightOut2, GPIO.OUT)
-  GPIO.setup(motorLeftOut1, GPIO.OUT)
-  GPIO.setup(motorLeftOut2, GPIO.OUT)
-  GPIO.output(motorRightOut1, GPIO.LOW)
-  GPIO.output(motorRightOut2, GPIO.LOW)
-  GPIO.output(motorLeftOut1, GPIO.LOW)
-  GPIO.output(motorLeftOut2, GPIO.LOW)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(motorRightOut1, GPIO.OUT)
+    GPIO.setup(motorRightOut2, GPIO.OUT)
+    GPIO.setup(motorLeftOut1, GPIO.OUT)
+    GPIO.setup(motorLeftOut2, GPIO.OUT)
+    GPIO.output(motorRightOut1, GPIO.LOW)
+    GPIO.output(motorRightOut2, GPIO.LOW)
+    GPIO.output(motorLeftOut1, GPIO.LOW)
+    GPIO.output(motorLeftOut2, GPIO.LOW)
+
 
 def cleanUp():
-  GPIO.cleanup()
+    GPIO.cleanup()
