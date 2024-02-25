31605237effd9b6b3a6f5dd27127a0dc6bca259e
generalpacific29 <prash.c.29@gmail.com>
2024-02-24 17:26:09 -0800

    [Paciframe] Add ability to change the resolution but maintaining the aspect ratio of the original image

--- Inky/paciframe.py
+++ Inky/paciframe.py
@@ -23,9 +23,33 @@ def __get_randomized_filenames(directory):
     return jpg_files
 
 
+"""
+Resizes the image to target_resolution maintaining the aspect ratio of the
+original image.
+"""
+def __resize_and_fill(image, target_resolution):
+    original_width, original_height = image.size
+    target_width, target_height = target_resolution
+
+    ratio = min(target_width / original_width, target_height / original_height)
+    new_width = int(original_width * ratio)
+    new_height = int(original_height * ratio)
+
+    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+
+    new_image = Image.new('RGB', target_resolution, (0, 0, 0))
+
+    x = (target_width - new_width) // 2
+    y = (target_height - new_height) // 2
+
+    new_image.paste(resized_image, (x, y))
+
+    return new_image
+
+
 def main():
     inky = auto(ask_user=True, verbose=True)
-    saturation = 0.9
+    saturation = 1
 
     jpg_files = __get_randomized_filenames(PACIFRAMEDIR)
     print("""There are {num} files""".format(num=len(jpg_files)))
@@ -35,7 +59,7 @@ def main():
             Displaying {jpg_file}
         """.format(jpg_file=jpg_file))
         image = Image.open(jpg_file)
-        resizedimage = image.resize(inky.resolution)
+        resizedimage = __resize_and_fill(image, inky.resolution) 
         inky.set_image(resizedimage, saturation=saturation)
         inky.show()
         time.sleep(REFRESH_INTERVAL_SEC)
